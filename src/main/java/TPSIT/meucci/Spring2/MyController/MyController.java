7cba35d2756a3ab68b6cbea05599fa28e982a209
lai2-24 <lai2-24@itismeucci.com>
2024-02-07 09:41:48 +0100

    updated

--- src/main/java/TPSIT/meucci/Spring2/MyController/MyController.java
+++ src/main/java/TPSIT/meucci/Spring2/MyController/MyController.java
@@ -3,7 +3,6 @@ package TPSIT.meucci.Spring2.MyController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import TPSIT.meucci.Spring2.model.Persona;
@@ -25,8 +25,13 @@ public class MyController {
     PersonaRepository personaRepository;
 
     @GetMapping("/list")
-    public List<Persona> list() {
-        return personaRepository.findAll();
+    public List<Persona> list(@RequestParam(name="name", required = false)String name) {
+        if (name!=null && name.trim().length()>0) {
+            return personaRepository.findByName(name);
+        }else{
+            return personaRepository.findAll();
+        }
+        
     }
 
     @GetMapping("/{id}")
--- src/main/java/TPSIT/meucci/Spring2/repository/PersonaRepository.java
+++ src/main/java/TPSIT/meucci/Spring2/repository/PersonaRepository.java
@@ -1,10 +1,12 @@
 package TPSIT.meucci.Spring2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import TPSIT.meucci.Spring2.model.Persona;
 
 public interface PersonaRepository extends JpaRepository<Persona, Long>{
 
-    
+    List<Persona> findByName(String name);
 }
